9692e6441ea0701df656c5ddf7f917ba6a53bed9
Arcueid <52120441+ArcueidShiki@users.noreply.github.com>
2024-12-28 14:29:58 +0800

    1. Add CImage cache 2.Update network image data to cache 3. Add Watch dlg resouces and code 4.TODO: Add Image data to Watch dlg
    Signed-off-by: Arcueid <52120441+ArcueidShiki@users.noreply.github.com>

--- RemoteCtrl/RemoteClient/RemoteClientDlg.h
+++ RemoteCtrl/RemoteClient/RemoteClientDlg.h
@@ -14,30 +14,38 @@ class CRemoteClientDlg : public CDialogEx
 // Construction
 public:
 	CRemoteClientDlg(CWnd* pParent = nullptr);	// standard constructor
-
 // Dialog Data
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_REMOTECLIENT_DIALOG };
 #endif
+	afx_msg void OnBnClickedBtnTest();
+	afx_msg void OnBnClickedBtnFileinfo();
+	afx_msg void OnNMDblclkTreeDir(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnNMClickTreeDir(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnNMRClickListFile(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnDownloadFile();
+	afx_msg void OnDeleteFile();
+	afx_msg void OnOpenFile();
+	afx_msg LRESULT OnSendPacket(WPARAM wParam, LPARAM lParam);
+	afx_msg void OnBnClickedBtnStartWatch();
+	BOOL isFull() const;
+	CImage& GetImage();
+	CListCtrl m_list;
+	CTreeCtrl m_tree;
+	CString m_port;
+	DWORD m_server_address;
 
-	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
-
-// Implementation
 protected:
-	HICON m_hIcon;
-	CStatusDlg m_dlgStatus;
 	// Generated message map functions
 	virtual BOOL OnInitDialog();
 	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
 	DECLARE_MESSAGE_MAP()
-public:
-	afx_msg void OnBnClickedBtnTest();
-	DWORD m_server_address;
-	CString m_port;
-	afx_msg void OnBnClickedBtnFileinfo();
+	HICON m_hIcon;
+	CStatusDlg m_dlgStatus;
+
 private:
 	int SendCommandPacket(int nCmd, BOOL autoclose = TRUE, BYTE* pData = NULL, size_t nLength = 0);
 	CString GetPath(HTREEITEM hTree);
@@ -49,17 +57,6 @@ private:
 	void ThreadDownloadFile();
 	void ThreadWatchData();
 	CClientSocket* pClient;
-	CImage m_img;	// cache
+	CImage m_img;
 	BOOL m_isFull;
-public:
-	CTreeCtrl m_tree;
-	afx_msg void OnNMDblclkTreeDir(NMHDR* pNMHDR, LRESULT* pResult);
-	afx_msg void OnNMClickTreeDir(NMHDR* pNMHDR, LRESULT* pResult);
-	// Display File
-	CListCtrl m_list;
-	afx_msg void OnNMRClickListFile(NMHDR* pNMHDR, LRESULT* pResult);
-	afx_msg void OnDownloadFile();
-	afx_msg void OnDeleteFile();
-	afx_msg void OnOpenFile();
-	afx_msg LRESULT OnSendPacket(WPARAM wParam, LPARAM lParam);
 };
---
+++ RemoteCtrl/RemoteClient/WatchDlg.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "afxdialogex.h"
+
+
+// CWatchDlg dialog
+
+class CWatchDlg : public CDialog
+{
+	DECLARE_DYNAMIC(CWatchDlg)
+
+public:
+	CWatchDlg(CWnd* pParent = nullptr);   // standard constructor
+	virtual ~CWatchDlg();
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DLG_WATCH };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnTimer(UINT_PTR nIDEvent);
+};
