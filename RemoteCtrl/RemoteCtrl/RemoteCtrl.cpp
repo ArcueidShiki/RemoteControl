0faebc1cc829f9b778cd32a280c9688d59ec1ca1
Arcueid <52120441+ArcueidShiki@users.noreply.github.com>
2025-01-05 14:50:48 +0800

    Add server to registry table, mk link boot startup
    Signed-off-by: Arcueid <52120441+ArcueidShiki@users.noreply.github.com>

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -19,14 +19,63 @@
 #pragma comment(linker, "/subsystem:console /entry:WinMainCRTStartup")
 #pragma comment(linker, "/subsystem:console /entry:mainCRTStartup")
 #endif
-
+// Linker->Manifest File->UAC Execution Level->requireAdministrator
+// Computer\\HKEY_LOCAL_MACHINE\\SOFTWARE\Microsoft\\Windows\\CurrentVersion\\Run For boot startup
 // The one and only application object
 
 CWinApp theApp;
 
-CServerSocket *pServer;
-
 using namespace std;
+
+void ChooseBootStartUp()
+{
+    CString warning = _T("This program is only allow legal usage!\n"
+                         "This machine will be under survelience if continue using this program!\n"
+                         "Click Cancel Button to exit, if you don't want to\n"
+                         "Click Yes, this program will copy to your mahine, and startup when booting\n"
+                         "Click No, this program will only run once, it won't left anything on your system");
+    int ret = MessageBox(NULL, warning, _T("Warnning"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
+    if (ret == IDYES)
+    {
+        char sPath[MAX_PATH] = "";
+		char sSysPath[MAX_PATH] = "";
+		GetCurrentDirectoryA(MAX_PATH, sPath);
+        GetSystemDirectoryA(sSysPath, MAX_PATH);
+		strcat_s(sPath, "\\RemoteCtrl.exe");
+        strcat_s(sSysPath, "\\RemoteControlServer.exe");
+        char sLinkCmd[MAX_PATH * 2] = "MKLINK ";
+        strcat_s(sLinkCmd, sSysPath);
+		strcat_s(sLinkCmd, " ");
+        strcat_s(sLinkCmd, sPath);
+		TRACE("Link Command: [%s]\n", sLinkCmd);
+        system(sLinkCmd);
+        CString strSubKey = _T("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run");
+        HKEY hKey = NULL;
+		// KEY_ALL_ACCESS | KEY_WOW64_64KEY
+        if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, strSubKey, 0, KEY_WRITE, &hKey) != ERROR_SUCCESS)
+        {
+            RegCloseKey(hKey);
+            MessageBox(NULL, _T("Set startup when booting failed. Don't have enough permission?\r\n Program start failed!"),
+                _T("Startup Failed"), MB_ICONERROR | MB_TOPMOST);
+            exit(0);
+        }
+        // using REG_EXPAND_SZ auto expand %path%
+		char sExePath[MAX_PATH] = "%SystemRoot%\\system32\\RemoteControlServer.exe";
+        TRACE("sExePath Path: [%s]\n", sExePath);
+        // RegSetValueExW(hKey, "RemoteCtrl", 0, REG_EXPAND_SZ, (BYTE*)(LPCTSTR)sExePath, sExePath.GetLength() * sizeof(THAR))
+		if (RegSetValueExA(hKey, "RemoteCtrl", 0, REG_EXPAND_SZ, (BYTE*)sExePath, (DWORD)strlen(sExePath)) != ERROR_SUCCESS)
+		{
+			MessageBox(NULL, _T("Set startup when booting failed. Don't have enough permission?\r\n Program start failed!"),
+				_T("Startup Failed"), MB_ICONERROR | MB_TOPMOST);
+		}
+        RegCloseKey(hKey);
+    }
+    else if (ret == IDCANCEL)
+    {
+        exit(0);
+    }
+    return;
+}
 
 int main()
 {
@@ -45,19 +94,17 @@ int main()
         }
         else
         {
+            ChooseBootStartUp();
             CCommand cmd;
-            // global variable, only one instance
-            pServer = CServerSocket::GetInstance();
-            int ret = pServer->Run(&CCommand::RunCommand, &cmd);
-            switch (ret)
+            switch (CServerSocket::GetInstance()->Run(&CCommand::RunCommand, &cmd))
             {
-            case -1:
-                MessageBox(NULL, _T("Cannot Initiate socket, please check network setting!"), _T("Soccket Initialization Error!"), MB_OK | MB_ICONERROR);
-                exit(0);
-                break;
-			case -2:
-                MessageBox(NULL, _T("Cannot accept user, failed many time, program exit"), _T("Accept Client Failed!"), MB_OK | MB_ICONERROR);
-                break;
+                case -1:
+                    MessageBox(NULL, _T("Cannot Initiate socket, please check network setting!"), _T("Soccket Initialization Error!"), MB_OK | MB_ICONERROR);
+                    exit(0);
+                    break;
+			    case -2:
+                    MessageBox(NULL, _T("Cannot accept user, failed many time, program exit"), _T("Accept Client Failed!"), MB_OK | MB_ICONERROR);
+                    break;
             }
         }
     }
